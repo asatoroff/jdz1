05a04f261ef3c268edcf781059c6cdd0f501af45
Дмитрий Асаторов <dmitrijasatorov@MacBook-Air-Dmitrij.local>
2021-01-08 13:46:22 +0300

    gitignore

---
+++ src/main.java
@@ -0,0 +1,8 @@
+public class main {
+    public static void main(String[] args) {
+      int score = 2_000_000_000;
+      int transfer = 500_000_000;
+      int total = score + transfer;
+      System.out.println(total);
+    }
+}
